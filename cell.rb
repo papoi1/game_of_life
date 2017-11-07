f7ed066b52e9107f0c9a9ccbf3f7b0a5d5d1304f
papoi1 <mikelxabier@gmail.com>
2017-11-07 13:30:10 +0100

    cells with new state logic and init world

--- cell.rb
+++ cell.rb
@@ -1,36 +1,34 @@
+
+
+class World
+  attr_reader :cols_number,  :rows_number
+
+  def initialize(cols, rows)
+  @cols_number=cols
+  @rows_number=rows
+  end
+
+end
+
 class Cell
-  MIN_NEIGHBOURS_TO_BE_ALIVE = 2
-  MAX_NEIGHBOURS_TO_BE_ALIVE = 3
-  NEIGHBOURS_TO_RESURRECT = 3
+  attr_reader :actual_status
 
-  def initialize(initial_status)
-    @actual_status = initial_status
-    @next_status = @actual_status
-    @live_neighbours = 0
+  def initialize(actual_status=:dead)
+    @actual_status=actual_status
+    @next_status=@actual_status
   end
 
-  def status
-    @actual_status
-  end
+  def check_next_status(number_of_neighbours)
+    if(@actual_status == :alive && (number_of_neighbours<2 || number_of_neighbours>3))
+      @next_status= :dead
+    end
 
-  def change_status
-    @next_status =  @actual_status == :alive ? :dead : :alive
-  end
-
-  def next_status
-    self.change_status?
+    if(@actual_status == :dead && number_of_neighbours == 3)
+      @next_status= :alive
+    end
 
     @next_status
-  end
 
-  def set_live_neighbours(live_cells)
-    @live_neighbours = live_cells
-  end
-
-  def change_status?
-    if (@actual_status == :alive && @live_neighbours < MIN_NEIGHBOURS_TO_BE_ALIVE) then self.change_status end
-    if (@actual_status == :alive && @live_neighbours > MAX_NEIGHBOURS_TO_BE_ALIVE) then self.change_status end
-    if (@actual_status == :dead && @live_neighbours == NEIGHBOURS_TO_RESURRECT) then self.change_status end
   end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,70 +1,53 @@
 require_relative '../cell.rb'
 
-describe "When a cell" do
-  context "is created with initial status" do
-    it "the status can be retrieved" do
-      cell = Cell.new(:alive)
-#sdfasdfasdfxzfgsdfgs aproach1 ready for commit
-#teteo
-      expect(cell.status).to eq(:alive)
-
-      cell = Cell.new(:dead)
-     p cell.class
-      expect(cell.status).to eq(:dead)
-    end
-
-    it "the next status can be set" do
-      cell = Cell.new(:alive)
-
-      cell.change_status
-
-      expect(cell.next_status).to eq(:dead)
-
-      cell = Cell.new(:dead)
-
-      cell.change_status
-
-      expect(cell.next_status).to eq(:alive)
-    end
+describe "A WORLD" do
+  it "has 2 dimensions" do
+    world= World.new(8,4)
+    expect(world.cols_number).to eq(8)
+    expect(world.rows_number).to eq(4)
   end
+end
 
-  context "is alive" do
-    it "with fewer than two live neighbours will die (underpopulation)" do
-      cell = Cell.new(:alive)
 
-      neighbours_for_underpopulation = 1
-      cell.set_live_neighbours(neighbours_for_underpopulation)
+describe "A cell" do
+    it "starts dead" do
+      cell = Cell.new
+      expect(cell.actual_status).to eq(:dead)
+    end
+    it "starts alive" do
+      cell= Cell.new(:alive)
+      expect(cell.actual_status).to eq(:alive)
+    end
+end
 
-      expect(cell.next_status).to eq(:dead)
+describe "Next status of a cell" do
+
+  context "who is alive" do
+
+    before(:each) do
+      @cell=Cell.new(:alive)
     end
 
-    it "with two or three live neighbours will be alive" do
-      cell = Cell.new(:alive)
-
-      cell.set_live_neighbours(3)
-
-      expect(cell.next_status).to eq(:alive)
+    it "will be DEAD with fewer than 2 live neigbours, as if caused by underpopulation" do
+      expect(@cell.check_next_status(1)).to eq(:dead)
     end
 
-    it "with more than three live neighbours will die (overcrowding)" do
-      cell = Cell.new(:alive)
+    it "will be DEAD with more than 3 live neigbours, as if caused by overcrowding" do
+      expect(@cell.check_next_status(4)).to eq(:dead)
+    end
 
-      min_neighbours_for_overcrowding = 4
-
-      cell.set_live_neighbours(min_neighbours_for_overcrowding)
-
-      expect(cell.next_status).to eq(:dead)
+    it "will continue ALIVE with 2 or 3 neigbours " do
+      expect(@cell.check_next_status(2)).to eq(:alive)
     end
 
   end
+  context "who is dead" do
+    before(:each) do
+      @dead_cell=Cell.new
+    end
 
-  context "is dead" do
-    it "with exactly three live neighbours becomes a live cell" do
-      cell = Cell.new(:dead)
-
-      cell.set_live_neighbours(3)
-
-      expect(cell.next_status).to eq(:alive)
+    it "will be ALIVE if has 3 live neigbours" do
+      expect(@dead_cell.check_next_status(3)).to eq(:alive)
     end
   end
 end
