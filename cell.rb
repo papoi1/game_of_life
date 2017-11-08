099702c8d534c542141e764311d47697d78c0e85
Manuel García de Quirós Moreno <manuelgqm@gmail.com>
2017-11-08 12:15:40 +0100

    add cell to world, refactor world context test and refactor set_next_status

--- cell.rb
+++ cell.rb
@@ -1,33 +1,40 @@
 
 
 class World
-  attr_reader :cols_number,  :rows_number
+  attr_reader :cells
 
-  def initialize(cols, rows)
-  @cols_number=cols
-  @rows_number=rows
+  def initialize(x_dimension, y_dimension)
+    @cells = Array.new(y_dimension) { Array.new(x_dimension) }
+  end
+
+  def dimensions
+    y_dimension = @cells.size
+    x_dimension = @cells[0].size
+    [x_dimension, y_dimension]
+  end
+
+  def add_cell(cell, x_dimension, y_dimension)
+    @cells[y_dimension][x_dimension] = cell
   end
 
 end
 
 class Cell
-  attr_reader :actual_status
+  attr_reader :actual_status, :next_status
 
-  def initialize(actual_status=:dead)
-    @actual_status=actual_status
-    @next_status=@actual_status
+  def initialize(actual_status = :dead)
+    @actual_status = actual_status
+    @next_status = @actual_status
   end
 
-  def check_next_status(number_of_neighbours)
-    if(@actual_status == :alive && (number_of_neighbours<2 || number_of_neighbours>3))
-      @next_status= :dead
+  def set_next_status(number_of_neighbours)
+    if(@actual_status == :alive && (number_of_neighbours < 2 || number_of_neighbours > 3))
+      return @next_status = :dead
     end
 
     if(@actual_status == :dead && number_of_neighbours == 3)
       @next_status= :alive
     end
-
-    @next_status
 
   end
 
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,21 +1,36 @@
 require_relative '../cell.rb'
 
 describe "A WORLD" do
+  before(:each) do
+    x_dimension = 4
+    y_dimension = 8
+    @world = World.new(x_dimension, y_dimension)
+  end
+  
   it "has 2 dimensions" do
-    world= World.new(8,4)
-    expect(world.cols_number).to eq(8)
-    expect(world.rows_number).to eq(4)
+    expect(@world.dimensions).to eq([4, 8])
+  end
+  
+  it "adds a cell" do
+    cell = {actual_status: :dead, next_status: :dead}
+    x_pos = 3
+    y_pos = 7
+    
+    @world.add_cell(cell, x_pos, y_pos)
+
+    expect(@world.cells[y_pos][x_pos]).to eq(cell)
   end
 end
-
 
 describe "A cell" do
     it "starts dead" do
       cell = Cell.new
+
       expect(cell.actual_status).to eq(:dead)
     end
     it "starts alive" do
-      cell= Cell.new(:alive)
+      cell = Cell.new(:alive)
+
       expect(cell.actual_status).to eq(:alive)
     end
 end
@@ -25,29 +40,37 @@ describe "Next status of a cell" do
   context "who is alive" do
 
     before(:each) do
-      @cell=Cell.new(:alive)
+      @cell = Cell.new(:alive)
     end
 
     it "will be DEAD with fewer than 2 live neigbours, as if caused by underpopulation" do
-      expect(@cell.check_next_status(1)).to eq(:dead)
+      @cell.set_next_status(1)
+
+      expect(@cell.next_status).to eq(:dead)
     end
 
     it "will be DEAD with more than 3 live neigbours, as if caused by overcrowding" do
-      expect(@cell.check_next_status(4)).to eq(:dead)
+      @cell.set_next_status(4)
+
+      expect(@cell.next_status).to eq(:dead)
     end
 
     it "will continue ALIVE with 2 or 3 neigbours " do
-      expect(@cell.check_next_status(2)).to eq(:alive)
+      @cell.set_next_status(2)
+
+      expect(@cell.next_status).to eq(:alive)
     end
 
   end
   context "who is dead" do
     before(:each) do
-      @dead_cell=Cell.new
+      @dead_cell = Cell.new
     end
 
     it "will be ALIVE if has 3 live neigbours" do
-      expect(@dead_cell.check_next_status(3)).to eq(:alive)
+      @dead_cell.set_next_status(3)
+
+      expect(@dead_cell.next_status).to eq(:alive)
     end
   end
 end
